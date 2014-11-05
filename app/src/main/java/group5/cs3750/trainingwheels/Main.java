b8093c640294436070e7e7fae010db29b3f6440d
Scott Stromberg <stromberg@Scotts-MacBook-Pro.local>
2014-11-04 19:26:56 -0700

    Initial commit

---
+++ app/src/main/java/group5/cs3750/trainingwheels/Main.java
@@ -0,0 +1,13 @@
+package group5.cs3750.trainingwheels;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Main extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
