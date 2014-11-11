7c56bb0ccd3ea24e1ca7f29ccedcb4fdd36985a1
Scott Stromberg <srstromberg@gmail.com>
2014-11-11 10:25:13 -0700

    updated tutorial layout

--- app/src/main/java/group5/cs3750/trainingwheels/Main.java
+++ app/src/main/java/group5/cs3750/trainingwheels/Main.java
@@ -9,5 +9,7 @@ public class Main extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
