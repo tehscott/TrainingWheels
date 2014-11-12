5c416e8280ffb6cf158f839a91bcd34acc7926f6
Zach <zbug_zlp@msn.com>
2014-11-12 14:08:36 -0700

    Initial commit for settings.java

---
+++ app/src/main/java/group5/cs3750/trainingwheels/Settings.java
@@ -0,0 +1,16 @@
+package group5.cs3750.trainingwheels;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Zachary on 11/12/2014.
+ */
+public class Settings extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+    }
+
+}
