6820117106cf9c7e1ae51a0f6339eb823cdfe0a2
jolsendev <jolsendev@gmail.com>
2014-12-01 14:24:01 -0700

    added different level examples to TrainingIDE added animation/ImageViews to Splash
    -Jamie

--- app/src/main/java/group5/cs3750/trainingwheels/TrainingIDE.java
+++ app/src/main/java/group5/cs3750/trainingwheels/TrainingIDE.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+
 public class TrainingIDE extends Activity{
     //
     Button bIf, bWhile, bFor, bString, bProcedure, bInt, bRun;
