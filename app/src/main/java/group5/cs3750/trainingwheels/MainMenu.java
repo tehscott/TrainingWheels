b12e51c56336ec255b79329f7fb1383944926a36
Scott Stromberg <srstromberg@gmail.com>
2014-12-02 11:20:16 -0700

    - added a background image - themed views a little - updated the main menu screen (buttons work now) - changed the app theme (style) to remove the action bar. we might want to add it back in later - made all screens landscape (in the manifest)

--- app/src/main/java/group5/cs3750/trainingwheels/MainMenu.java
+++ app/src/main/java/group5/cs3750/trainingwheels/MainMenu.java
@@ -1,7 +1,10 @@
 package group5.cs3750.trainingwheels;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Brady on 11/13/2014.
@@ -11,6 +14,41 @@ public class MainMenu extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        initButtons();
     }
 
+    private void initButtons() {
+        findViewById(R.id.btnSettings).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent settings = new Intent(MainMenu.this, Settings.class);
+                startActivity(settings);
+            }
+        });
+
+        findViewById(R.id.btnContinueGame).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent user = new Intent(MainMenu.this, TrainingIDE.class);
+                startActivity(user);
+            }
+        });
+
+        findViewById(R.id.btnStartNewGame).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent user = new Intent(MainMenu.this, TrainingIDE.class);
+                startActivity(user);
+            }
+        });
+
+        findViewById(R.id.btnStats).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent user = new Intent(MainMenu.this, User.class);
+                startActivity(user);
+            }
+        });
+    }
 }
